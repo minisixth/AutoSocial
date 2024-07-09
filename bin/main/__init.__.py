7eff61b6a2414a65d96af1fd8b810acf06c7f0a4
MiniSixth - 小六子 <sevice.sixtou@gmail.com>
2024-07-09 13:13:05 +0000

    [Add]Main主程式框架

---
+++ bin/main/__init.__.py
@@ -0,0 +1,15 @@
+###自動化帳號系統
+'''
+作者 Author: MiniSixth 小六子
+Email: sixtou@pm.me
+版本: v0.1.0 beta
+說明:
+    本程式僅供學習及測試用途，不負任何使用上法律責任。
+'''
+
+def main():
+    #主程序Main Function
+    print("歡迎使用自動化帳號系統")
+
+if __name__ == '__main__':
+    main()
---
+++ bin/main/env_check.py
@@ -0,0 +1,8 @@
+def check_online_module():
+    print("檢查線上套件是否安裝")
+
+def check_self_module():
+    print("檢查自製套件是否安裝")
+
+def check_sql():
+    print("檢查SQL使用")
